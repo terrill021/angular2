058e5de9d4786386e882e43b1b670c7542093711
Alejandro <aterill021@gmail.com>
2017-07-22 00:00:20 -0500

    tour of heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'master-key';
+  title = 'tour of heroes';
+  hero : Hero = {
+      id : 1,
+    name : 'Alejandro'
+  }
+}
+export class Hero {
+  id: number;
+  name: string;
 }

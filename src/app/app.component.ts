00dd30be731a79a4f677ce08a877c2f9b9472f65
Alejandro <aterill021@gmail.com>
2017-07-23 23:33:09 -0500

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,38 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {Hero} from './Hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   // properties
   title = 'tour of heroes';
-  heroes = HEROES;
+  heroes: Hero [];
   selectedHero: Hero;
+  // Constructor
+  constructor(private heroService: HeroService) {
+  }
+
+  // Life cicle
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   // Methods
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-}
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' }
-];
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes =>
+      this.heroes = heroes
+    );
+  }
+
+}
